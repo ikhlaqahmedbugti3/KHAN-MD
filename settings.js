8c14dc9badad0d12bec5996ff4ce1fc11ba8230b
ikhlaqahmedbugti3 <ikhlaqahmedbugti3@gmail.com>
2026-01-11 00:55:02 +0500

    Update OWNER_NUMBER and PREFIX in settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@
 module.exports = {
   SESSION_ID: "",  // add session Id 
   
-  OWNER_NUMBER: "923427582273", // add owner number 
+  OWNER_NUMBER: "923334104708", // add owner number 
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "*", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Asia/Karachi" // put your country timeZone....leave blank if u don't know.
 };
